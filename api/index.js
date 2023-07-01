0dfc4f7598c83a2f35283c57e3c8391c53a8f7a6
Naman Agarwal <naman.ag41402@gmail.com>
2023-07-01 13:23:35 +0530

    updated cors

--- api/index.js
+++ api/index.js
@@ -35,6 +35,18 @@ app.use(
     origin: process.env.BASE_URL,
   })
 );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 app.use(
   fileUpload({
     useTempFiles: true,
